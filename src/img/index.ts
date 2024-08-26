ec14e9e68bb11d1c50bce27e896dde2cc86f38a2
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-08-26 23:28:42 +0200

    replace img logos and style dropdown menu

--- src/img/index.ts
+++ src/img/index.ts
@@ -1,7 +1,7 @@
 import ameriqube from './logos/logoAMERIqube.png';
 import ameriqubeText from './logos/logoAMERIqubeText.png';
-import icequbeLogo from './logos/logoAMERIqubeText.png';
-import smartqubeLogo from './logos/logoAMERIqubeText.png';
+import icequbeLogo from './logos/AmeriQube_IceQube-White.png';
+import smartqubeLogo from './logos/AmeriQube_SmartQube-White.png';
 
 export const photos = {
   ameriqube,
