fe22cf76eec4aacd58d592d4e781e3b0103ed2af
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-08-28 08:31:35 +0200

    Create hook useLanguageAndRegionCookie to improve handling of language and region settings.
    - Added type checks for language and region using LanguagesType and RegionType.
    - Used useCallback to optimize the changeLanguage function.
    - Implemented cookies to save language and region preferences for 1 year.
    - Ensured initial values are correctly set from cookies or i18n defaults.

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { Language } from '../types/navigation-types';
 import enTranslation from './en/en.json';
 import plTranslation from './pl/pl.json';
 import deTranslation from './de/de.json';
@@ -18,7 +19,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'en',
+  lng: Language.En,
   fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
