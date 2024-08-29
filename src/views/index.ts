f9d2cf1110793a225e976e841b8acb65883ad03f
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-08-29 10:33:18 +0200

    Updated the App component to include routing for all pages, ensuring proper handling of non-matchinUg routes.

---
+++ src/views/index.ts
@@ -0,0 +1,6 @@
+export { AboutPage } from './AboutPage/AboutPage';
+export { ContactPage } from './ContactPage/ContactPage';
+export { CookiesPage } from './CookiesPage/CookiesPage';
+export { HomePage } from './HomePage/HomePage';
+export { NotFoundPage } from './NotFoundPage/NotFoundPage';
+export { PrivacyPage } from './PrivacyPage/PrivacyPage';
