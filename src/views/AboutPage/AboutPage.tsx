01fe6ee9f91680bcc0d3f9e6260b806db6427d31
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-08-29 13:22:46 +0200

    add splash mockup to about page

--- src/views/AboutPage/AboutPage.tsx
+++ src/views/AboutPage/AboutPage.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Container } from '../../layout';
+import styles from './AboutPage.module.scss';
+import {
+  SplashHeading,
+  LineOrnament,
+  SplashSubHeading,
+} from '../../components/index';
 
 export const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <section className={styles.splash}>
+      <Container className={styles.carouselWrap}>
+        <SplashHeading />
+        <LineOrnament />
+        <SplashSubHeading />
+      </Container>
+    </section>
+  );
 };
