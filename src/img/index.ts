21d03023b2cc874fc50014d7a1a6aff427212c7f
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-08-30 19:31:00 +0200

    Added custom TypeScript declaration for .mp4 files to resolve import issues and ensure correct handling of video files in the project.

--- src/img/index.ts
+++ src/img/index.ts
@@ -1,11 +1,16 @@
-import ameriqube from '../public/logos/logoAMERIqube.png';
+import ameriqube from './logos/logoAMERIqube.png';
 import ameriqubeText from './logos/logoAMERIqubeText.png';
 import icequbeLogo from './logos/AmeriQube_IceQube-White.png';
 import smartqubeLogo from './logos/AmeriQube_SmartQube-White.png';
+import videoBg from './video/videoBg.mp4';
 
 export const photos = {
   ameriqube,
   ameriqubeText,
   icequbeLogo,
   smartqubeLogo,
+};
+
+export const videos = {
+  videoBg,
 };
