d1f0ae44a4bbc00ae3abb13ee891850da61f8187
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-09-02 18:41:59 +0200

    Moved SwiperSplash to the layout section to clean up the HomePage component.

--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -4,51 +4,21 @@ import {
   ContactBanner,
   ProductsSection,
   SwiperSplash,
+  SwiperSplashHome,
 } from '../../layout';
-import { useTranslation } from 'react-i18next';
-import { Slide } from '../../types/homePage-types';
-import {
-  ButtonRed,
-  ButtonTransparent,
-  LineOrnament,
-  Paragraph,
-  SectionHeader,
-} from '../../components';
+
 import styles from './HomePage.module.scss';
 
 export const HomePage = () => {
-  const { t } = useTranslation();
-
-  const slides: Slide[] = t('pages.home.swiper', {
-    returnObjects: true,
-  });
-
   return (
     <div className={styles.main}>
-      <SwiperSplash className={styles.swiperSplash}>
-        {slides.map((slide, index) => (
-          <div key={index} className={styles.slide}>
-            <SectionHeader
-              text={slide.title}
-              className={styles.sectionHeader}
-            />
-            <LineOrnament className={styles.ornament} />
-            <Paragraph text={slide.intro} className={styles.paragraph} />
-            <div className={styles.buttons}>
-              <ButtonRed to={slide.buttonRed[0]} text={slide.buttonRed[1]} />
-              {slide.buttonTransparent && (
-                <ButtonTransparent
-                  to={slide.buttonTransparent[0]}
-                  text={slide.buttonTransparent[1]}
-                />
-              )}
-            </div>
-          </div>
-        ))}
-      </SwiperSplash>
+      <SwiperSplashHome />
       <AmeriQubeWelcome />
       <ProductsSection />
       <ContactBanner />
+      <SwiperSplash className={styles.iceQubeProducts}>
+        <p>dd</p>
+      </SwiperSplash>
     </div>
   );
 };
