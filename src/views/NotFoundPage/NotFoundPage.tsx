dc60d40486eb44cb1544857be4d9fa2a41ffad96
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-09-05 16:03:11 +0200

    add not foundPage

--- src/views/NotFoundPage/NotFoundPage.tsx
+++ src/views/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { UnderConstruction } from '../UnderConstruction/UnderConstruction';
+import styles from './NotFoundPage.module.scss';
+import { Container } from '../../layout';
 
-export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
-};
+export const NotFoundPage = () => (
+  <>
+    <div className={styles.blackBackground}></div>
+    <div className={styles.notFound}>
+      <Container>
+        <h3 className={styles.error}>Ooops... 404</h3>
+      </Container>
+      <UnderConstruction />;
+    </div>
+  </>
+);
