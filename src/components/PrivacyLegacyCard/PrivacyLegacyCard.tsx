d1c12641208b787dd572d43cfcbf658f4b587291
OllaWilk <aleksandra.wilk.letkiewicz@gmail.com>
2024-09-05 22:57:04 +0200

    add mockup for pfivacy view

---
+++ src/components/PrivacyLegacyCard/PrivacyLegacyCard.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { PiCookieLight, PiFilePdfDuotone } from 'react-icons/pi';
+import styles from './PrivacyLegacyCard.module.scss';
+import { Paragraph, Subtitle } from '../Typography';
+import { Link } from 'react-router-dom';
+import { LineOrnament } from '../LineOrnament/LineOrnament';
+
+interface Props {
+  title: string;
+  description: string;
+  downloadLink?: string;
+  url?: string;
+  btnTExt: string;
+}
+
+export const PrivacyLegacyCard = ({
+  title,
+  description,
+  downloadLink,
+  url,
+  btnTExt,
+}: Props) => {
+  return (
+    <article key={`document-${title}`} className={styles.card}>
+      <div className={styles.cartContent}>
+        <div className={styles.logos}>
+          {url ? <PiCookieLight /> : <PiFilePdfDuotone />}
+          <div className={styles.titleWrap}>
+            <Subtitle text={title} />
+            <LineOrnament />
+          </div>
+        </div>
+        <Paragraph text={description} />
+      </div>
+      <div className={styles.btnDownloadWrap}>
+        {url ? (
+          <Link
+            to={url}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.downloadLink}
+          >
+            {btnTExt}
+          </Link>
+        ) : (
+          <a
+            href={downloadLink}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.downloadLink}
+          >
+            {btnTExt}
+          </a>
+        )}
+      </div>
+    </article>
+  );
+};
